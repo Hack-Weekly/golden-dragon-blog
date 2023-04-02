1b4df1f3e7cf630355e24650597ddeffda1a3888
Sydney Nguyen <melosydrose@gmail.com>
2023-04-02 00:44:42 -0400

    remove duplicate class

--- server/blog/post/models.py
+++ server/blog/post/models.py
@@ -27,13 +27,3 @@ class PostCategory(models.Model):
 
     def __str__(self):
         return self.name
-
-
-
-class PostCategory(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
