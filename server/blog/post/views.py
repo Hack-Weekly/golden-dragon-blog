3e2bd2a0880aeb10a62a48eced87cb17a3dde085
Sydney Nguyen <melosydrose@gmail.com>
2023-04-02 22:23:50 -0400

    allow read for all but write only for own content

--- server/blog/post/views.py
+++ server/blog/post/views.py
@@ -1,34 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    message = 'Editing posts is restricted to the author'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user == self.request.user
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
     def get_queryset(self):
-        if self.request.user.is_staff:
-            return Post.objects.all()
-        return Post.objects.filter(user=self.request.user)
+        return Post.objects.all()
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
     def get_queryset(self):
-        if self.request.user.is_staff:
-            return Comment.objects.all()
-        return Comment.objects.filter(user=self.request.user)
+        return Comment.objects.all()
+    
