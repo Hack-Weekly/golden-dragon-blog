127a45ceb826b5f1fbaf3b5f1f3b6ce1936bac1d
taddymason <67209663+kingl0w@users.noreply.github.com>
2023-04-07 16:53:31 -0700

    Update BlogList.jsx

--- client/src/routes/BlogList.jsx
+++ client/src/routes/BlogList.jsx
@@ -1,7 +1,16 @@
+import BlogPosts1 from "../components/BlogPosts1";
+import BlogPosts2 from "../components/BlogPosts2";
+import FeaturedPost from "../components/FeaturedPost";
+
+
+
 function BlogList() {
   return (
     <>
-      <h1>Blog List</h1>
+      <h1 className="title">Dream Blog</h1>
+        <BlogPosts1 />
+        <FeaturedPost />
+        <BlogPosts2 />
     </>
   );
 }
